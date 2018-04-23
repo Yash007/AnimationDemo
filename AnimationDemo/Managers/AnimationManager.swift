356d1011e6983e6368bd8c638cb6772008f9d900
Yash007 <sompurayash@live.com>
2018-04-23 19:12:42 -0400

    Button Added and View control created

---
+++ AnimationDemo/Managers/AnimationManager.swift
@@ -0,0 +1,28 @@
+
+import CoreGraphics
+
+class AnimationManager  {
+    //Top
+    class var Top:CGPoint   {
+        // TODO: return Top Value
+        return CGPoint(x: 10.0, y: 10.0)
+    }
+    
+    //Right
+    class var Right:CGPoint   {
+        // TODO: return Top Value
+        return CGPoint(x: 10.0, y: 10.0)
+    }
+    
+    //Bottom
+    class var Bottom:CGPoint   {
+        // TODO: return Top Value
+        return CGPoint(x: 10.0, y: 10.0)
+    }
+    
+    //Left
+    class var Left:CGPoint   {
+        // TODO: return Top Value
+        return CGPoint(x: 10.0, y: 10.0)
+    }
+}
